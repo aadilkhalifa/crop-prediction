fb934167e0e7757fa05bca178572315351e03b58
aadilkhalifa <aadilkhalifa@gmail.com>
2021-11-22 16:12:19 +0300

    integrated with backend

--- server/server.py
+++ server/server.py
@@ -1,10 +1,48 @@
-from flask import Flask
+from __future__ import print_function
+from flask import Flask, request, jsonify
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import classification_report
+from sklearn import metrics
+from sklearn import tree
+import pickle
+import requests
+import warnings
+warnings.filterwarnings('ignore')
+# from __future__ import print_function # In python 2.7
+import sys
+from json import *
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/crop")
+crop_recommendation_model_path = './XGBoost.pkl'
+crop_recommendation_model = pickle.load(
+    open(crop_recommendation_model_path, 'rb'))
+
+@app.route("/crop", methods=['POST'])
 def members():
-    return {"crop": "Jute"}
+
+    N = int(request.json['N'])
+    P = int(request.json['P'])
+    K = int(request.json['K'])
+    ph = float(request.json['Ph'])
+    temprature = float(request.json['temprature'])
+    humidity = float(request.json['humidity'])
+    rainfall = float(request.json['rainfall'])
+
+
+    data = np.array([[N, P, K, temprature, humidity, ph, rainfall]])
+
+    # data = np.array([[104,18, 30, 23.603016, 60.3, 6.7, 140.91]])
+    # data = np.array([[83, 45, 60, 28, 70.3, 7.0, 150.9]])
+    my_prediction = crop_recommendation_model.predict(data)
+    final_prediction = my_prediction[0]
+
+    return jsonify({"crop": final_prediction})
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/Components/Home/TrialSection.jsx
+++ src/Components/Home/TrialSection.jsx
@@ -2,20 +2,54 @@ import './TrialSection.scss'
 
 import React, {useState, useEffect} from 'react'
 
+
 function TrialSection() {
 
-    const [data, setData] = useState([{}]);
+    const [data, setData] = useState("");
 
-    useEffect(() => {
-        fetch("/crop").then(
-            res => res.json()
-        ).then(
-            data => {
-                setData(data)
+    var N_value;
+    var P_value;
+    var K_value;
+    var Ph_value;
+    var Temp_value;
+    var Humidity_value;
+    var Rainfall_value;
+
+    function getData(){
+
+        const requestOptions = {
+            // mode:'no-cors',
+            dataType: 'json',
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ 
+                N: N_value, 
+                P: P_value, 
+                K: K_value, 
+                Ph: Ph_value,  
+                temprature: Temp_value,  
+                humidity: Humidity_value,  
+                rainfall: Rainfall_value,  
+                // N: '83', 
+                // P: '45', 
+                // K: '60', 
+                // Ph: '28',  
+                // temprature: '70.3',  
+                // humidity: '7',  
+                // rainfall: '150.9',  
+            })
+        };
+        
+        fetch('http://localhost:5000/crop', requestOptions)
+            .then(response => response.json())
+            .then(data => {
                 console.log(data)
-            }
-        )
-    }, [])
+                setData(data.crop)
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }
 
     return (
         <div className="section trailSection">
@@ -28,29 +62,52 @@ function TrialSection() {
                 </h3>
             </div>
             <div className="trailSectionRight">
-                <form action="">
+                <form action="http://localhost:5000/crop" method="POST">
                     <h3>Soil Data</h3>
                     <div className="inputRow">
                         <div className="inputDiv">
-                            <label htmlFor="">Nitrogen value</label>
-                            <input type="text" placeholder="Enter value" />
+                            <label htmlFor="nitrogen">Nitrogen value</label>
+                            <input name="nitrogen" type="text" placeholder="Enter value" 
+                                onChange={(value)=>N_value=value.target.value} />
                         </div>
                         <div className="inputDiv">
-                            <label htmlFor="">Phosphorus value</label>
-                            <input type="text" placeholder="Enter value" />
+                            <label htmlFor="phosphorus">Phosphorus value</label>
+                            <input name="phosphorus" type="text" placeholder="Enter value"
+                                onChange={(value)=>P_value=value.target.value} />
                         </div>
                     </div>
                     <div className="inputRow">
                         <div className="inputDiv">
-                            <label htmlFor="">Potassium value</label>
-                            <input type="text" placeholder="Enter value" />
+                            <label htmlFor="potassium">Potassium value</label>
+                            <input name="potassium" type="text" placeholder="Enter value"
+                                onChange={(value)=>K_value=value.target.value} />
                         </div>
                         <div className="inputDiv">
-                            <label htmlFor="">Ph value</label>
-                            <input type="text" placeholder="Enter value"  />
+                            <label htmlFor="ph">Ph value</label>
+                            <input name="ph" type="text" placeholder="Enter value"
+                                onChange={(value)=>Ph_value=value.target.value}  />
                         </div>
                     </div>
                     <div className="inputRow">
+                        <div className="inputDiv">
+                            <label htmlFor="temprature">Temprature value</label>
+                            <input name="temprature" type="text" placeholder="Enter value"
+                                onChange={(value)=>Temp_value=value.target.value} />
+                        </div>
+                        <div className="inputDiv">
+                            <label htmlFor="humidity">Humidity value</label>
+                            <input name="humidity" type="text" placeholder="Enter value"
+                                onChange={(value)=>Humidity_value=value.target.value}  />
+                        </div>
+                    </div>
+                    <div className="inputRow">
+                        <div className="inputDiv">
+                            <label htmlFor="rainfall">Rainfall value</label>
+                            <input name="rainfall" type="text" placeholder="Enter value"
+                                onChange={(value)=>Rainfall_value=value.target.value} />
+                        </div>
+                    </div>
+                    {/* <div className="inputRow">
                         <div className="inputDiv">
                             <h3>Location Data</h3>
                             <label htmlFor="">Location</label>
@@ -61,11 +118,14 @@ function TrialSection() {
                             <label htmlFor="">Time Period</label>
                             <input type="text" placeholder="Enter value"  />
                         </div>
-                    </div>
-                    <button>Submit</button>
-                    <div className="resultDiv">
-                        Recommeded Crop: Jute
-                    </div>
+                    </div> */}
+                    <button onClick={ getData } type="button">Submit</button>
+                    {
+                        data !== "" ?
+                            <div className="resultDiv">
+                                Recommeded Crop: {data}
+                            </div> : null
+                    }
                 </form>
             </div>
         </div>
